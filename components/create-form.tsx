673a1f65d0de3f72398aad815c857729cc128e08
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-12-02 10:51:37 +0700

    Feat: Add Action

--- components/create-form.tsx
+++ components/create-form.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import React from "react";
+import { uploadImage } from "@/lib/actions";
+import { useFormState } from "react-dom";
 
 const CreateForm = () => {
+  const [state, formAction] = useFormState(uploadImage, null);
   return (
-    <form action="">
+    <form action={formAction}>
       <div className="mb-4 pt-2">
         <input
           type="text"
@@ -10,6 +15,9 @@ const CreateForm = () => {
           className="py-2 px-4 w-full border border-gray-200 rounded-lg text-sm outline-none focus:outline-blue-500 disabled:opacity-50 disabled:pointer-events-none"
           placeholder="Title..."
         />
+        <div aria-live="polite" aria-atomic="true">
+          <p className="text-sm text-red-500 mt-2">{state?.error?.title}</p>
+        </div>
       </div>
       <div className="mb-4 pt-2">
         <input
@@ -17,6 +25,9 @@ const CreateForm = () => {
           name="image"
           className="w-full border border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 outline-none focus:outline-blue-500 disabled:opacity-50 disabled:pointer-events-none file:bg-gray-50 file:border-0 file:me-4 file:py-2 file:px-4"
         />
+        <div aria-live="polite" aria-atomic="true">
+          <p className="text-sm text-red-500 mt-2">{state?.error?.image}</p>
+        </div>
       </div>
       <div className="mb-4 pt-4">
         <button className="bg-blue-600 text-white w-full font-medium py-2.5 px-6 text-base rounded-lg hover:bg-blue-500">
