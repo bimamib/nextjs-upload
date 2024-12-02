4dd3e44812d3f82166f2a7c655e685804ee98303
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-12-02 14:39:46 +0700

    Next Config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "7u3rnz8d1mz8ef0n.public.blob.vercel-storage.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
