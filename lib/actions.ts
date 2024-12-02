30acafbfc791543fa5109e97fafb5e5e0393be37
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-12-02 10:51:14 +0700

    Feat: Validate Database

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,12 +1,16 @@
 "use server";
 import { z } from "zod";
+import { put } from "@vercel/blob";
+import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 const UploadSchema = z.object({
   title: z.string().min(1),
   image: z
     .instanceof(File)
     .refine((file) => file.size > 0, { message: "Image is required" })
-    .refine((file) => file.type.startsWith("image/png"), {
+    .refine((file) => file.size === 0 || file.type.startsWith("image/png"), {
       message: "Only images are allowed",
     })
     .refine((file) => file.size < 4000000, {
@@ -24,4 +28,24 @@ export const uploadImage = async (prevState: unknown, formData: FormData) => {
       error: validatedFields.error.flatten().fieldErrors,
     };
   }
+
+  const { title, image } = validatedFields.data;
+  const { url } = await put(image.name, image, {
+    access: "public",
+    multipart: true,
+  });
+
+  try {
+    await prisma.upload.create({
+      data: {
+        title,
+        image: url,
+      },
+    });
+  } catch (error) {
+    return { message: "Failed to create data" };
+  }
+
+  revalidatePath("/");
+  redirect("/");
 };
