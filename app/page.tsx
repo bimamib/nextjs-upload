f4c16c1e32a8ae8327beb02450914302435fb693
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-12-03 16:02:38 +0700

    Fix Styling Page Home

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,19 @@ export default async function Home() {
   const images = await getImages();
 
   return (
-    <div className="max-w-screen-lg mx-auto py-14">
-      <div className="flex items-end justify-between">
-        <h1 className="text-4xl font-bold">Latest Image</h1>
-        <Link
-          href="/create"
-          className="py-3 px-6 bg-blue-600 hover:bg-blue-500 text-white rounded-lg"
-        >
-          Upload New Image
-        </Link>
+    <div className="max-w-screen-lg mx-auto py-14 px-4 sm:px-6 lg:px-8">
+      <div className="flex flex-wrap items-center justify-between gap-4">
+        <h1 className="text-3xl sm:text-4xl font-bold">Latest Image</h1>
+        <div className="w-full sm:w-auto flex justify-center sm:justify-end">
+          <Link
+            href="/create"
+            className="w-full text-center py-2 px-4 sm:py-3 sm:px-6 bg-blue-600 hover:bg-blue-500 text-sm sm:text-base text-white rounded-lg"
+          >
+            Upload New Image
+          </Link>
+        </div>
       </div>
-      <div className="grid md:grid-cols-3 gap-5 mt-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-10">
         {images.map((item) => (
           <Card key={item.id} data={item} />
         ))}
